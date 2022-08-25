dc586f91c4744afff22865e3010e79131a73f7f3
Gerrit <info@gerritschwerthelm.de>
2022-08-25 11:59:29 +0200

    Re-Use cloud init generator from oscommon.

--- pkg/generator/ignition/generator.go
+++ pkg/generator/ignition/generator.go
@@ -1,59 +1,18 @@
 package ignition
 
 import (
-	"bytes"
 	"fmt"
-	"path"
 	"text/template"
 
 	"github.com/gardener/gardener/extensions/pkg/controller/operatingsystemconfig/oscommon/generator"
+	ostemplate "github.com/gardener/gardener/extensions/pkg/controller/operatingsystemconfig/oscommon/template"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
-	"github.com/gardener/gardener/pkg/utils"
 )
-
-// DefaultUnitsPath is the default CoreOS path where to store units at.
-const DefaultUnitsPath = "/etc/systemd/system"
-
-type fileData struct {
-	Path              string
-	Content           string
-	Dirname           string
-	Permissions       *string
-	TransmitUnencoded *bool
-}
-
-type unitData struct {
-	Path    string
-	Name    string
-	Content *string
-	DropIns *dropInsData
-}
-
-type dropInsData struct {
-	Path  string
-	Items []*dropInData
-}
-
-type dropInData struct {
-	Path    string
-	Content string
-}
-
-type initScriptData struct {
-	CRI              *extensionsv1alpha1.CRIConfig
-	Files            []*fileData
-	Units            []*unitData
-	Bootstrap        bool
-	Type             string
-	AdditionalValues map[string]interface{}
-}
 
 // IgnitionGenerator generates cloud-init scripts.
 type IgnitionGenerator struct {
-	ignitionTemplate *template.Template
-	unitsPath        string
-	cmd              string
-
+	cloudInitGenerator   generator.Generator
+	cmd                  string
 	additionalValuesFunc func(*extensionsv1alpha1.OperatingSystemConfig) (map[string]interface{}, error)
 }
 
@@ -70,95 +29,14 @@ func (t *IgnitionGenerator) Generate(data *generator.OperatingSystemConfig) ([]b
 		return data, cmd, err
 	}
 
-	var tFiles []*fileData
-	for _, file := range data.Files {
-		tFile := &fileData{
-			Path:              file.Path,
-			Dirname:           path.Dir(file.Path),
-			TransmitUnencoded: file.TransmitUnencoded,
-		}
-		if file.TransmitUnencoded != nil && *file.TransmitUnencoded {
-			tFile.Content = string(file.Content)
-		} else {
-			tFile.Content = utils.EncodeBase64(file.Content)
-		}
-		if file.Permissions != nil {
-			permissions := fmt.Sprintf("%04o", *file.Permissions)
-			tFile.Permissions = &permissions
-		}
-		tFiles = append(tFiles, tFile)
-	}
-
-	var tUnits []*unitData
-	for _, unit := range data.Units {
-		var content *string
-		if unit.Content != nil {
-			encoded := utils.EncodeBase64(unit.Content)
-			content = &encoded
-		}
-		tUnit := &unitData{
-			Name:    unit.Name,
-			Path:    path.Join(t.unitsPath, unit.Name),
-			Content: content,
-		}
-		if len(unit.DropIns) != 0 {
-			dropInPath := path.Join(t.unitsPath, fmt.Sprintf("%s.d", unit.Name))
-
-			var items []*dropInData
-			for _, dropIn := range unit.DropIns {
-				items = append(items, &dropInData{
-					Path:    path.Join(dropInPath, dropIn.Name),
-					Content: utils.EncodeBase64(dropIn.Content),
-				})
-			}
-			tUnit.DropIns = &dropInsData{
-				Path:  dropInPath,
-				Items: items,
-			}
-		}
-
-		tUnits = append(tUnits, tUnit)
-	}
-
-	initScriptData := &initScriptData{
-		Type:      data.Object.Spec.Type,
-		CRI:       data.CRI,
-		Files:     tFiles,
-		Units:     tUnits,
-		Bootstrap: data.Bootstrap,
-	}
-
-	if t.additionalValuesFunc != nil {
-		additionalValues, err := t.additionalValuesFunc(data.Object)
-		if err != nil {
-			return nil, nil, err
-		}
-		initScriptData.AdditionalValues = additionalValues
-	}
-
-	var buf bytes.Buffer
-	if err := t.ignitionTemplate.Execute(&buf, initScriptData); err != nil {
-		return nil, nil, err
-	}
-
-	return buf.Bytes(), cmd, nil
+	return t.cloudInitGenerator.Generate(data)
 }
 
 // NewIgnitionGenerator creates a new CloudInitGenerator with the given units path.
 func NewIgnitionGenerator(template *template.Template, unitsPath string, cmd string, additionalValuesFunc func(*extensionsv1alpha1.OperatingSystemConfig) (map[string]interface{}, error)) *IgnitionGenerator {
 	return &IgnitionGenerator{
-		ignitionTemplate:     template,
-		unitsPath:            unitsPath,
+		cloudInitGenerator:   ostemplate.NewCloudInitGenerator(template, unitsPath, cmd, additionalValuesFunc),
 		cmd:                  cmd,
 		additionalValuesFunc: additionalValuesFunc,
 	}
-}
-
-// NewTemplate creates a new template with the given name.
-func NewTemplate(name string) *template.Template {
-	return template.New(name).Funcs(template.FuncMap{
-		"isContainerDEnabled": func(cri *extensionsv1alpha1.CRIConfig) bool {
-			return cri != nil && cri.Name == extensionsv1alpha1.CRINameContainerD
-		},
-	})
 }
